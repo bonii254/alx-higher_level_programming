ac383a6c594e31eb5ca41e85745fbaf21daf9902
bonface murangiri <bonnyrangi95@gmail.com>
2023-08-08 21:09:18 +0000

    To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-	for letter in str:
-		if 96 < ord(letter) < 123:
-			letter = chr(ord(letter) - 32)
-		print(f"{letter}", end = "")
-	print("")
+    for letter in str:
+        if 96 < ord(letter) < 123:
+            letter = chr(ord(letter) - 32)
+        print(f"{letter}", end = "")
+    print("")
