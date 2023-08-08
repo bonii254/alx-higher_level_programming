0b2ebe77828d62c45069eb33785d646704694078
bonface murangiri <bonnyrangi95@gmail.com>
2023-08-08 22:05:14 +0000

    00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(100):
     if num == 99:
-        print(num)
+        print("{}".format(num))
     else:
-        print(f"{num:02}", end = ", ")
+        print("{:02}".format(num), end=", ")
