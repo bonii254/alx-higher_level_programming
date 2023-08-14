c8791c3d705c40639990954d6e062407c874ef22
bonface murangiri <bonnyrangi95@gmail.com>
2023-08-14 12:54:44 +0000

    More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    my_tuple = tuple(sentence)
-    if my_tuple == ()
+    if sentence == "":
         return 0, None
-    return len(my_tuple), my_tuple[0]
+    return len(sentence), sentence[0]
