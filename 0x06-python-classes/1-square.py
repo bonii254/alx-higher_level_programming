43cb32c3f348d5ab819df08b859e8c6c890c21c6
bonface murangiri <bonnyrangi95@gmail.com>
2023-08-30 19:32:37 +0000

    My first square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Defines a class called square"""
+
+
+class Square:
+    """defines the class square"""
+
+    def __init__(self, size):
+        """Initialize a square.
+
+        Args:
+            size (int): len of square"""
+        self.__size = size
