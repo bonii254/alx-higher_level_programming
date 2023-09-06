1d3f90c650a1bfd951945677380ddeb639b82a90
bonface murangiri <bonnyrangi95@gmail.com>
2023-09-06 14:29:19 +0000

    Detect instance deletion

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+"""Defines a rectangle class."""
+
+
+class Rectangle:
+    """This class defines a rectangle with customizable width and height
+        attributes."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a Rectangle with optional width and height.
+        Args:
+            width (int): Width of the new rectangle
+            height (int): Height of new rectangle
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Get the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """returns the rectangle area"""
+        return (self.__height * self.__width)
+
+    def perimeter(self):
+        """returns the rectangle perimeter"""
+        if (self.__height == 0 or self.__width == 0):
+            return 0
+        else:
+            return ((self.__height * 2) + (self.__width * 2))
+
+    def __str__(self):
+        """print the rectangle with the character #"""
+        if (self.__height == 0 or self.__width == 0):
+            return ""
+
+        rectobj = []
+        for col in range(self.__height):
+            for row in range(self.__width):
+                rectobj.append("#")
+            if col != self.__height - 1:
+                rectobj.append('\n')
+        return ("".join(rectobj))
+
+    def __repr__(self):
+        """a string representation of the rectangle to be able to recreate
+            a new instance by using eval()
+        """
+        rect = "Rectangle(" + str(self.__width)
+        rect += ", " + str(self.__height) + ")"
+        return (rect)
+
+    def __del__(self):
+        """delete instance of Rectangle"""
+        print("Bye rectangle...")
