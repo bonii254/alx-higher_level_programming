1238244b44478b8b715056f49efa4c913b865009
bonface murangiri <bonnyrangi95@gmail.com>
2023-09-11 15:33:28 +0000

    Exact same object

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""check if obj is instance of the specified class"""
+
+
+def is_same_class(obj, a_class):
+    """check if obj is instance of the specified class"""
+    return type(obj) is a_class
