1c9bc70593f9cfea1941ebef232a013fb46f2981
bonface murangiri <bonnyrangi95@gmail.com>
2023-09-13 01:26:40 +0000

    Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" writes a string to a text file (UTF8) and returns the number of 
+characters written"""
+
+
+def write_file(filename="", text=""):
+    """ writes a string to a text file (UTF8) and returns the number of
+    characters written"""
+    with open(text, mode='w', encoding='utf8') as wf:
+            size = wf.write(text)
+            return size;
