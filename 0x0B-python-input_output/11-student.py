f30ada75f8032016273989af1d07359baf9663ed
bonface murangiri <bonnyrangi95@gmail.com>
2023-09-13 20:05:43 +0000

    Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Defines a student class"""
+
+
+class Student:
+    """Represents a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes a student
+
+        Args:
+            first_name (str): students first name
+            last_name (str): students last name
+            age (int): student age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Returns serialization description(dictionary)
+
+        Args:
+            obj: an instance of a class
+        """
+        if (type(attrs) is list and all(type(item) is str for item in attrs)):
+            return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
+        return self.__dict_
+
+    def reload_from_json(self, json):
+        """Reloads from JSON
+        Args
+            json (dict): keyvalue pairs rep attributes
+        """
+        for k, v in json.items():
+            setattr(self, k, v)
