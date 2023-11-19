a2ad546900446e24d275136d6ab0d0a5351da9fd
bonface murangiri <bonnyrangi95@gmail.com>
2023-11-19 01:02:10 -0800

    Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv
+    mysql_username = args[1]
+    mysql_password = args[2]
+    database_name = args[3]
+
+    connection = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_password,
+            db=database_name,
+            charset="utf8",
+    )
+    cursor = connection.cursor()
+
+    sql = "SELECT * FROM states WHERE name like 'N%' ORDER BY id ASC"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+
+    for row in results:
+        print(row)
+
+    cursor.close()
+    connection.close()
